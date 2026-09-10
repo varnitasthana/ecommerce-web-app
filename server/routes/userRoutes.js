322cc92df46436685eff4d43d6edbfacd0689bb3
varnitasthana <varnitasthanayt@gmail.com>
2026-09-10 23:34:08 +0530

    feat: add user routes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,9 +1,13 @@
 const express = require("express");
-const { updateUserRole } = require("../controllers/userController");
+const { updateUserRole, getCurrentUser } = require("../controllers/userController");
+const { getRecentlyViewed, addRecentlyViewed } = require("../controllers/recentlyViewedController");
 const { protect, adminOnly } = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
 router.patch("/:id/role", protect, adminOnly, updateUserRole);
+router.get("/me", protect, getCurrentUser);
+router.get("/recently-viewed", protect, getRecentlyViewed);
+router.post("/recently-viewed", protect, addRecentlyViewed);
 
 module.exports = router;
