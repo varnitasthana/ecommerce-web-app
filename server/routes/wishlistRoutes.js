cfff16d54d925772d5a29a9708304881ae78f5a8
varnitasthana <varnitasthanayt@gmail.com>
2026-09-10 23:34:09 +0530

    feat: enhance wishlist routes

--- server/routes/wishlistRoutes.js
+++ server/routes/wishlistRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { getWishlist, toggleWishlist } = require("../controllers/wishlistController");
+const { getWishlist, toggleWishlist, getPriceAlert, togglePriceAlert } = require("../controllers/wishlistController");
 const { protect } = require("../middleware/authMiddleware");
 
 const router = express.Router();
@@ -7,5 +7,7 @@ const router = express.Router();
 router.use(protect);
 router.get("/", getWishlist);
 router.post("/:productId/toggle", toggleWishlist);
+router.get("/:productId/price-alert", getPriceAlert);
+router.post("/:productId/price-alert", togglePriceAlert);
 
 module.exports = router;
