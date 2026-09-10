75f2ce808a91b9dd1c1c6f9bb714e4fe78ccdfdc
varnitasthana <varnitasthanayt@gmail.com>
2026-09-10 23:34:07 +0530

    feat: enhance payment routes

--- server/routes/paymentRoutes.js
+++ server/routes/paymentRoutes.js
@@ -1,12 +1,11 @@
 const express = require("express");
-const { createCheckoutSession, handleStripeWebhook, getCheckoutSession, createDemoCheckoutSession } = require("../controllers/paymentController");
+const { createRazorpayOrder, verifyRazorpayPayment, handleRazorpayWebhook, createDemoCheckoutSession } = require("../controllers/paymentController");
 const { protect } = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
-router.post("/create-checkout-session", protect, createCheckoutSession);
+router.post("/create-order", protect, createRazorpayOrder);
+router.post("/verify", protect, verifyRazorpayPayment);
 router.post("/demo-checkout", protect, createDemoCheckoutSession);
-router.get("/session", protect, getCheckoutSession);
-router.post("/webhook", express.raw({ type: "application/json" }), handleStripeWebhook);
 
 module.exports = router;
